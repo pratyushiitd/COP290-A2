c0a15c7559ad13f071333c1ab9fb153fbbb69f36
pratyushiitd <pratyushiitd@github.com>
2021-05-30 01:13:58 +0530

    Minor Bugs corrected

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,10 @@ int main(int argc, char *argv[]){
 
     game = new Game();
     game->init("Game Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, false);
-    std::cout << "Game Initialization Successful..." << std::endl;
+    std::cout << "Game Initialization Successfull..." << std::endl;
     map = new Map();
     map->LoadMaze();
-    std::cout << "Maze Loading Successful..." << std::endl;
+    std::cout << "Maze Loading Successfull..." << std::endl;
     map->DrawMap(game->gameRenderer);
     SDL_RenderPresent(game->gameRenderer);
     while(game->isRunning()){
